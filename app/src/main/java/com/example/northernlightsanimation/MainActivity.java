7d45e66ab2c369f27e956b102613be5459dc76b8
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-03-29 19:36:45 -0700

    buttons given stop start functionality

--- app/src/main/java/com/example/northernlightsanimation/MainActivity.java
+++ app/src/main/java/com/example/northernlightsanimation/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.northernlightsanimation;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,39 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    AnimationDrawable lightsAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        ImageView imgFrame = (ImageView)findViewById(R.id.imgLights);
+        imgFrame.setBackgroundResource(R.drawable.animation);
+        lightsAnimation = (AnimationDrawable)imgFrame.getBackground();
+
+        Button button1 = (Button)findViewById(R.id.btnStart);
+        Button button2 = (Button)findViewById(R.id.btnStop);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lightsAnimation.start();
+            }
         });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lightsAnimation.stop();
+            }
+        });
+
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
