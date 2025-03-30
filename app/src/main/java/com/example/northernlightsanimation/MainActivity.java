cada751e259b07342fe7224350d8f9e6bcf4b215
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-03-29 19:57:48 -0700

    project animation complete

--- app/src/main/java/com/example/northernlightsanimation/MainActivity.java
+++ app/src/main/java/com/example/northernlightsanimation/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.northernlightsanimation;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
@@ -38,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 lightsAnimation.stop();
+                startActivity(new Intent(MainActivity.this, Tween.class));
             }
         });
 
